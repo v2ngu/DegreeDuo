b54ecf5fdf73392129e756b1c3524df134c3a185
Vincent <vincent854321@gmail.com>
2024-05-27 16:59:36 -0500

    Login Component
    Login Component w/o styling

---
+++ src/Login.jsx
@@ -0,0 +1,19 @@
+function Login(){
+    return(
+        <>
+            <div className = "title-container">
+                <button className = "button">Back</button>
+                <form action="login" method = "post">
+                    <label htmlFor="username">Username</label>
+                    <input type="text" id = "username" name ="username"/>
+                    <label htmlFor="password">Password</label>
+                    <input type="password" id="password" name = "password"/>
+                    <button type = "submit">Login</button>
+                    
+                </form>
+            </div>
+        </>
+    )
+}
+
+export default Login
