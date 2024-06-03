daa9a41502a521040f34f4994361f81b58ef4696
KevinH005 <kevinhnguyen005@gmail.comm>
2024-06-03 18:05:06 -0500

    Scraper Done

--- scrapers/courses_scraper.py
+++ scrapers/courses_scraper.py
@@ -1,44 +1,68 @@
 import json
 
-# Current askes for input in terminal will probably change to another input source
-course_name = input("Enter the name of the course: ")
+# Define the Course class
+class Course:
+    def __init__(self, department, course_id, course_name, day, hour, room, instruction_mode, instructor_name, status, flags, level, cc):
+        self.department = department
+        self.course_id = course_id
+        self.course_name = course_name
+        self.day = day
+        self.hour = hour
+        self.room = room
+        self.instruction_mode = instruction_mode
+        self.instructor_name = instructor_name
+        self.status = status
+        self.flags = flags
+        self.level = level
+        self.cc = cc
 
-# Initialize variables that will probably be accessed later
-course_id = "N/a"
-course_name = "N/a"
-course_day = "N/a"
-course_hour = "N/a"
-course_room = "N/a"
-course_instruct_mode = "N/a"
-course_instruct_name = "N/a"
-course_status = "N/a"
-course_flags = "N/a"
+    @classmethod
+    def from_json(cls, data):
+        return cls(
+            data["Department"],
+            data["Course ID"],
+            data["Course Name"],
+            data["Day"],
+            data["Hour"],
+            data["Room"],
+            data["Instruction Mode"],
+            data["Instructor Name"],
+            data["Status"],
+            data["Flags"],
+            data["Level"],
+            data{"Core Curriculum"}
+        )
 
-# Course Data Scraper -------------------------------------------------------
+# Read data from the JSON file
+with open("coursesdata.json") as f:
+    courses_data = json.load(f)
 
-# Open the JSON file and load its contents
-with open('courses.json') as f:
-    data = json.load(f)
+# Create Course objects from the JSON data and add them to the list
+courses = [Course.from_json(course) for course in courses_data]
 
-# Search for the course in the JSON data
-found_course = None
-for course in data:
-    if course['Course Name'] == course_name:
-        found_course = course
-        break
+# Print the list of courses
+for course in courses:
+    print(course.__dict__)
 
-# Print the information if the course is found
-if found_course:
-    course_id = found_course['Course ID']
-    course_name = found_course['Course Name']
-    course_day = found_course['Day']
-    course_hour = found_course['Hour']
-    course_room = found_course['Room']
-    course_instruct_mode = found_course['Instruction Mode']
-    course_instruct_name = found_course['Instructor Name']
-    course_status = found_course['Status']
-    course_flags = found_course['Flags']
+search_option = input("Search Option: ")
+results = []
+
+if search_option == "Department and Level":
+    department_input = input("Department: ")
+    level_input = input("Level: ")
+    for course in courses:
+        if course.department == department_input and course.level == level_input:
+            results.append(course)
+elif search_option == "Core Curriculum and Flags":
+    cc_input = input("Core Curriculum: ")
+    flags_input = input("Flags: ")
+    for course in courses:
+        if course.cc == cc_input and flags_input in course.flags:
+            results.append(course)
+elif search_option == "Unique Number":
+    unique_number = input("Unique Number: ")
+    for course in courses:
+        if course.course_id == unique_number:
+            results.append(course)
 else:
-    print("Course not found.")
-    
-# ---------------------------------------------------------------------------   
+    print("Search Option Error")
